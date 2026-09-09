c43e9e403e61e255ed582d254e221f2db1d42f97
ryftgihj-ai <ryftgihj@gmail.com>
2026-09-09 15:12:41 +0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,26 @@
+# ===== НАСТРОЙКИ БОТА =====
 BOT_TOKEN = "8997532043:AAEcTl6VKnS4Rtg3LyrDkfBFvqSzXqnVTLI"
 ADMIN_ID = 5633209888
 
+# ===== НАСТРОЙКИ X-UI =====
 XRAY_API = "http://135.106.185.216:49531/panel/api/inbounds"
 XRAY_API_TOKEN = "Yo4lmswupTdn6QZvy03tdD0IpCOFJRMExpAmGvKo1ok7CnU4"
 SERVER_IP = "135.106.185.216"
+
+# ===== ID ФОТО =====
+PHOTO_START = "AgACAgIAAxkBAAEujr9qoUwQEoZWPHyIlC3LLOTVk3-vEQACmh9rG8akEEkuWDy4F5kCCgEAAwIAA3MAAz0E"
+PHOTO_SUPPORT = "AgACAgIAAxkBAAEujsFqoUxGPcXQf7fs0bZJp7uh0NPslQACoB9rG8akEEkGH0N-0abEWQEAAwIAA3MAAz0E"
+PHOTO_TRIAL = "AgACAgIAAxkBAAEujsNqoUxs05SvbzxEo3z_VBG5WjtbGwACoR9rG8akEElImoVm6r3iDwEAAwIAA3MAAz0E"
+PHOTO_BUY = "AgACAgIAAxkBAAEujs1qoUyw5eYAATbPVmSSLQWYs7LUJEsAAqQfaxvGpBBJTL4yGIqHvJYBAAMCAANzAAM9BA"
+
+# ===== ССЫЛКИ =====
+HELP_URL = "https://telegra.ph/Instrukciya-po-podklyucheniyu-k-VPN-09-09"
+SUPPORT_LINK = "https://t.me/pepsi_water"
+
+# ===== ЦЕНЫ =====
+PRICES = {
+    "30": 120,
+    "90": 350,
+    "180": 1000,
+    "365": 2000
+}
